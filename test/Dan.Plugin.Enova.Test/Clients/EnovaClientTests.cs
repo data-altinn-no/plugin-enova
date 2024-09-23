310805063f509bea490ff8fdb339c86ca02aef4b
Sondre Jahrsengene <sondre.jahrsengene@digdir.no>
2024-09-23 15:36:40 +0200

    test: add unit test with proper mocked http call

--- test/Dan.Plugin.Enova.Test/Clients/EnovaClientTests.cs
+++ test/Dan.Plugin.Enova.Test/Clients/EnovaClientTests.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading;
@@ -11,26 +13,37 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Moq;
 using Newtonsoft.Json;
+using RichardSzalay.MockHttp;
 
 namespace Dan.Plugin.Enova.Test.Clients;
 
 public class EnovaClientTests
 {
+    private readonly MockHttpMessageHandler _mockHttpHandler;
     private readonly Mock<IDistributedCache> _distributedCacheMock;
-
     private readonly EnovaClient _enovaClient;
+
+    private  const string BaseAddress = "https://mock.mock-enova.mock";
 
     public EnovaClientTests()
     {
+        _mockHttpHandler  = new MockHttpMessageHandler();
+        var settings = new Settings{EnovaUrl = BaseAddress};
         Mock<IHttpClientFactory> clientFactoryMock = new();
-        Mock<IOptions<Settings>> settingsMock = new();
-
+        Mock<IOptions<Settings>> settingsOptions = new();
         Mock<ILoggerFactory> loggerFactoryMock = new();
 
         _distributedCacheMock = new Mock<IDistributedCache>();
+
+        clientFactoryMock
+            .Setup(x => x.CreateClient(It.IsAny<string>()))
+            .Returns(_mockHttpHandler.ToHttpClient());
+
+        settingsOptions.Setup(x => x.Value).Returns(settings);
+
         _enovaClient = new EnovaClient(
             clientFactoryMock.Object,
-            settingsMock.Object,
+            settingsOptions.Object,
             loggerFactoryMock.Object,
             _distributedCacheMock.Object);
     }
@@ -85,6 +98,66 @@ public class EnovaClientTests
 
         // Assert
         actual.Should().BeEquivalentTo(emsCsv);
+    }
+
+    [Fact]
+    public async Task GetEnergyPublicData_CacheNotFound_ShouldFetchFromSource()
+    {
+        // Arrange
+        const int year = 2023;
+        const string organizationNumber = "org";
+        const string mockFilePath = "https://mock.mock-enova.mock/mock-file-path";
+        var fullUrl = $"{BaseAddress}/ems/offentlige-data/v1/Fil/{year}";
+
+        var falseArray = "false"u8.ToArray();
+        var isCachedKey = $"{PluginConstants.SourceName}-EmsCsv-{year}-IsCached";
+        _distributedCacheMock
+            .Setup(m => m.GetAsync(isCachedKey, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(falseArray);
+
+        const string csv = "Knr,Gnr,Bnr,Snr,Fnr,Andelsnummer,Bygningsnummer,GateAdresse,Postnummer,Poststed,BruksEnhetsNummer,Organisasjonsnummer,Bygningskategori,Byggear,Energikarakter,Oppvarmingskarakter,Utstedelsesdato,TypeRegistrering,Attestnummer,BeregnetLevertEnergiTotaltkWhm2,BeregnetFossilandel,Materialvalg,HarEnergiVurdering,EnergiVurderingDato\n" +
+                           "1234,11,22,33,44,,11223344,Mockvei 123,1234,MOCK,H0101,org,Småhus,1976,E,Yellow,2023-12-31T23:45:20.0000000,Simple,randomguidgoeshere,224.93,\"0,61\",Tre,False,";
+        _mockHttpHandler
+            .When(fullUrl)
+            .Respond("application/json", $"{{'bankFileUrl': '{mockFilePath}'}}");
+
+        _mockHttpHandler
+            .When(mockFilePath)
+            .Respond("application/octet-stream", csv);
+
+        var expected = new EmsResponseModel
+        {
+            Kommunenummer = 1234,
+            Gaardsnummer = "11",
+            Bruksnummer = "22",
+            Seksjonsnummer = "33",
+            Festenummer = "44",
+            Andelsnummer = "",
+            Bygningsnummer = "11223344",
+            GateAdresse = "Mockvei 123",
+            Postnummer = 1234,
+            Poststed = "MOCK",
+            BruksEnhetsNummer = "H0101",
+            Organisasjonsnummer = "org",
+            Bygningskategori = "Småhus",
+            Byggear = 1976,
+            Energikarakter = "E",
+            Oppvarmingskarakter = "Yellow",
+            Utstedelsesdato = DateTime.Parse("2023-12-31T23:45:20.0000000"),
+            TypeRegistrering = "Simple",
+            Attestnummer = "randomguidgoeshere",
+            BeregnetLevertEnergiTotaltkWhm2 = 224.93,
+            BeregnetFossilandel = "0,61",
+            Materialvalg = "Tre",
+            HarEnergiVurdering = false,
+            EnergiVurderingDato = null
+        };
+
+        // Act
+        var actual = await _enovaClient.GetEnergyPublicData(year, organizationNumber);
+
+        // Assert
+        actual.FirstOrDefault().Should().BeEquivalentTo(expected);
     }
 
     [Fact]
