b36b1e406cf771cbef6826f287c36ea53d64e23c
PERHOF <pernille.hofgaard@hotmail.com>
2026-09-16 07:56:57 +0200

    added open telemetry

--- src/Dan.Plugin.Enova/Program.cs
+++ src/Dan.Plugin.Enova/Program.cs
@@ -1,5 +1,7 @@
 using Azure.Core;
 using Azure.Identity;
+using Azure.Monitor.OpenTelemetry.Exporter;
+using Microsoft.Azure.Functions.Worker.OpenTelemetry;
 using Microsoft.Extensions.Hosting;
 using Dan.Common.Extensions;
 using Dan.Plugin.Enova.Clients;
@@ -7,7 +9,9 @@ using Dan.Plugin.Enova.Config;
 using Dan.Plugin.Enova.Mappers;
 using Dan.Plugin.Enova.Models;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using OpenTelemetry.Trace;
 using StackExchange.Redis;
 
 var host = new HostBuilder()
@@ -17,8 +21,27 @@ var host = new HostBuilder()
         // Add more configuration sources if necessary. ConfigureDanPluginDefaults will load environment variables, which includes
         // local.settings.json (if developing locally) and applications settings for the Azure Function
     })
+    .ConfigureLogging(logging =>
+    {
+        logging.AddOpenTelemetry(options =>
+        {
+            options.IncludeFormattedMessage = true;
+            options.IncludeScopes = true;
+        });
+    })
     .ConfigureServices((context, services) =>
     {
+        // native OpenTelemetry running alongside the classic AI SDK from ConfigureDanPluginDefaults() in Dan.Common.
+        // We will get double logging in Application Insights for now, but this is a POC to validate that OpenTelemetry works before we remove the classic AI SDK in Dan.Common.
+        var appInsightsConnectionString = context.Configuration["APPLICATIONINSIGHTS_CONNECTION_STRING"];
+        if (!string.IsNullOrWhiteSpace(appInsightsConnectionString))
+        {
+            services.AddOpenTelemetry()
+                .WithTracing(tracing => tracing.AddHttpClientInstrumentation())
+                .UseAzureMonitorExporter(options => options.ConnectionString = appInsightsConnectionString);
+            services.AddOpenTelemetry().UseFunctionsWorkerDefaults();
+        }
+
         // Add any additional services here
         services.AddTransient<IEnovaClient, EnovaClient>();
         services.AddTransient<IMapper<EmsCsv, EmsResponseModel>, EmsResponseModelMapper>();
